f03f85afb0adafcc20b3aaec9c23a1b397089277
Pratik-sys <pithore.pratik@gmail.com>
2020-03-27 16:54:35 +0530

    Revert "Uprev Soundtrigger to 2.2"
    This reverts commit 13c3dfb3a25a37eb1865bc08f882f1761f141fff.

--- sdm660.mk
+++ sdm660.mk
@@ -101,8 +101,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
+    android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.bluetooth.audio@2.0 \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
