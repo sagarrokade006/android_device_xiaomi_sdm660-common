9a4e102bf54ff16b2bd08e5267e0cf998abd9e82
Akhil MS <51810638+AkhilmsAchu@users.noreply.github.com>
2020-03-31 17:09:59 +0530

    Added camera props from stock Miui 11

--- properties.mk
+++ properties.mk
@@ -58,21 +58,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi \
-    persist.vendor.bokeh.switch.lux=290 \
     persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.depth.focus.cb=0 \
-    persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.imglib.usefdlite=1 \
-    persist.vendor.camera.isp.clock.optmz=0 \
-    persist.vendor.camera.isp.turbo=1 \
-    persist.vendor.camera.linkpreview=0 \
     persist.vendor.camera.preview.ubwc=0 \
     persist.vendor.camera.stats.test=0 \
-    persist.vendor.imx376_ofilm.light.lux=275 \
-    persist.vendor.imx376_ofilm.low.lux=290 \
-    persist.vendor.imx376_sunny.light.lux=275 \
-    persist.vendor.imx376_sunny.low.lux=290 \
+    persist.vendor.camera.depth.focus.cb=0 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.linkpreview=0 \
+    persist.vendor.camera.isp.turbo=1 \
+    persist.vendor.camera.exif.make=Xiaomi \
+    persist.vendor.camera.exif.model=Redmi Note 6 Pro \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.ltm.overlap=13 \
+    persist.vendor.camera.fdvideo=1 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi
 
 # Codec2 switch
